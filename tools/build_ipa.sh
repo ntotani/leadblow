179ba5b34d92552bd8a605ceb15e4cd927046b6c
Naoyuki Totani <n_totani@septeni-original.co.jp>
2014-06-16 19:26:28 +0900

    fix build tools

--- tools/build_ipa.sh
+++ tools/build_ipa.sh
@@ -6,4 +6,4 @@ PROJ=leadblow
 MPROV=leadblow_AdHoc.mobileprovision
 
 xcodebuild -sdk iphoneos7.1 -project $PROJ.xcodeproj -scheme leadblow -configuration $CONF $JOB CODE_SIGN_IDENTITY="iPhone Distribution: Maho Tsutsui (4VK3MQNCVY)"
-xcrun -sdk iphoneos7.1 PackageApplication -v $DIR/DerivedData/$PROJ/Build/Products/$CONF-iphoneos/$PROJ_iOS.app -o $DIR/DerivedData/$PROJ/Build/Products/$CONF-iphoneos/${PROJ}_iOS.ipa --embed $DIR/$MPROV --sign "iPhone Distribution: Maho Tsutsui (4VK3MQNCVY)"
+xcrun -sdk iphoneos7.1 PackageApplication -v $DIR/DerivedData/$PROJ/Build/Products/$CONF-iphoneos/${PROJ}_iOS.app -o $DIR/DerivedData/$PROJ/Build/Products/$CONF-iphoneos/${PROJ}_iOS.ipa --embed $DIR/$MPROV --sign "iPhone Distribution: Maho Tsutsui (4VK3MQNCVY)"
