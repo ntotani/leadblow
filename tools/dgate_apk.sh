e7a6ccf0ddbaa576a1af68be76ef181739d945fb
Naoyuki Totani <n_totani@septeni-original.co.jp>
2014-07-14 12:20:39 +0900

    fix deploy tool

--- tools/dgate_apk.sh
+++ tools/dgate_apk.sh
@@ -1,3 +1,6 @@
-curl -F "file=@frameworks/runtime-src/proj.android/bin/leadblow-release.apk" \
+CONF=$1
+
+curl -F "file=@frameworks/runtime-src/proj.android/bin/leadblow-${CONF}.apk" \
      -F "token=dd1e7dd4e30de752e3db487b9fa96ab9990adf5e" \
+     -F "message=$CONF" \
      https://deploygate.com/api/users/sepgame/apps
--- tools/dgate_ipa.sh
+++ tools/dgate_ipa.sh
@@ -1,3 +1,6 @@
-curl -F "file=@frameworks/runtime-src/proj.ios_mac/DerivedData/leadblow/Build/Products/Release-iphoneos/leadblow iOS.ipa" \
+CONF=$1
+
+curl -F "file=@frameworks/runtime-src/proj.ios_mac/DerivedData/leadblow/Build/Products/$CONF-iphoneos/leadblow_iOS.ipa" \
      -F "token=dd1e7dd4e30de752e3db487b9fa96ab9990adf5e" \
+     -F "message=$CONF" \
      https://deploygate.com/api/users/sepgame/apps
